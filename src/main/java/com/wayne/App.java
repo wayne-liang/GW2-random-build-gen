833d818c3f139541d7f81ae2fe655575afecd8dd
TacticalSandwich <wlia631@aucklanduni.ac.nz>
2017-09-06 22:53:54 +1200

    Created project

---
+++ src/main/java/com/wayne/App.java
@@ -0,0 +1,13 @@
+package com.wayne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
